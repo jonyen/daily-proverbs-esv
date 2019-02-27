88d5e156eb6d0fdc1e57fb65a4dc73a9bd481b55
Jonathan Yen <jyen@cvent.com>
2019-02-27 06:25:14 -0500

    initial commit

---
+++ proverb.py
@@ -0,0 +1,42 @@
+#!/usr/bin python
+
+import requests
+import datetime
+import re
+
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from authentication import login, app_password, API_KEY
+
+API_URL = 'https://api.esv.org/v3/passage/html/'
+
+d = datetime.datetime.today()
+
+params = { 
+  'q': 'Proverbs %s' % d.day }
+
+headers = {
+  'Authorization': 'Token %s' % API_KEY 
+}
+
+data = requests.get(API_URL, params=params, headers=headers).json()
+
+html = re.sub('\s+', ' ', data['passages'][0]).strip()
+
+me = "jonyen@gmail.com"
+you = "jonyen@gmail.com"
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Proverbs %s" % d.day
+msg['From'] = me
+msg['To'] = you
+
+msg.attach(MIMEText(html, 'html'))
+
+s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+s.login(login, app_password)
+s.sendmail(me, you, msg.as_string())
+s.quit()
