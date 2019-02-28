63fc51dc4de720ae78cc2b53f0e4b064c5a2e884
EC2 Default User <ec2-user@ip-172-31-40-28.ec2.internal>
2019-02-28 14:49:31 +0000

    encode html text with utf-8

--- proverb.py
+++ proverb.py
@@ -26,7 +26,7 @@ data = requests.get(API_URL, params=params, headers=headers).json()
 
 html = re.sub('\s+', ' ', data['passages'][0]).strip()
 
-me = "jonyen@gmail.com"
+me = "Daily Proverb - ESV"
 you = "jonyen@gmail.com"
 
 msg = MIMEMultipart('alternative')
@@ -34,7 +34,7 @@ msg['Subject'] = "Proverbs %s" % d.day
 msg['From'] = me
 msg['To'] = you
 
-msg.attach(MIMEText(html, 'html'))
+msg.attach(MIMEText(html.encode("utf-8"), 'html'))
 
 s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 s.login(login, app_password)
